f17f1751d5468618cb54ca75e7257df041096f30
Reilly <rroan55@gmail.com>
2019-07-26 10:56:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var em = document.createElement('em'); // new element
+var para = document.querySelector('p'); // reference existing p
+
+em.textContent = "You're my boy blue";
+
+para.appendChild(em);
+
